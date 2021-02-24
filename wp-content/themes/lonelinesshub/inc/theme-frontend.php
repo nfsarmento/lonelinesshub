6f25e6f47732addfb2d0b36287f13d2a57769fe2
Nuno <nuno@nuno-sarmento.com>
2021-02-24 12:45:20 +0000

    Plugin disable WP email activation
    Add plugin to disable WP email activation. This will allow the user to be register automatically after filling up the registration form via an email invite.

--- wp-content/themes/lonelinesshub/inc/theme-frontend.php
+++ wp-content/themes/lonelinesshub/inc/theme-frontend.php
@@ -17,3 +17,36 @@ function get_excerpt_trim($num_words='8', $more=' ...'){
     $excerpt = wp_trim_words( $excerpt, $num_words , $more );
     return $excerpt;
 }
+
+
+/*
+ * 
+ * Add script on footer for website rgistration form
+ * 
+*/
+add_action('wp_footer', 'lonelinesshub_function_name', 9999);
+function lonelinesshub_function_name(){
+?>
+<script>
+jQuery( document ).ready( function($){
+	$( ".register-section-logo.private-on-div" ).empty();
+});		
+</script>
+<style>
+.register-section-logo.private-on-div {
+	background-image: url(/wp-content/uploads/2021/02/hub-logo.png) !important;
+	width: 320px !important;
+	background-size: 310px !important;
+	height: 150px;
+	background-repeat: no-repeat;
+}
+.register .site-content {
+	background: #ffffff;
+}	
+#privacy-modal header,
+#terms-modal header{
+    display: none !important;
+}	
+</style>
+<?php
+};
