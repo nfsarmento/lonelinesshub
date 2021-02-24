996a77d320738a23ce7b379db3736baabc4732bd
Nuno <nuno@nuno-sarmento.com>
2021-02-24 12:48:27 +0000

    Amend login widget style and T&C modal pages
    Amend login widget style and T&C modal pages

--- wp-content/themes/lonelinesshub/assets/js/custom.js
+++ wp-content/themes/lonelinesshub/assets/js/custom.js
@@ -1,11 +1,9 @@
 /* This is your custom Javascript */
 
-// (function($) {
-// $('a.header-search-link').append('<span class="search-icon">Search<span>');
-
-// })( jQuery );
-
-jQuery( document ).ready( function(){
-    //$('a.header-search-link').append('<span class="search-icon">Search<span>');
-	//$('.messages-wrap a').append('Message');
+jQuery( document ).ready( function($){
+    $('a.header-search-link').append('<span class="search-icon">Search<span>');
+	$('#header-messages-dropdown-elem .notification-link').append('<span class="msg-icon">Messages<span>');
+	$('#header-notifications-dropdown-elem .notification-link').append('<span class="notification-icon">Notifications<span>');
+	$(".logged-in .bbp_widget_login h3").html('You are logged in');
 });
+
