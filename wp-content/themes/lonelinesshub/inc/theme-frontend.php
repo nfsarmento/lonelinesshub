156c6f45651933f75e36d3fba4eb1b218af88caa
Nuno <nuno@nuno-sarmento.com>
2021-02-26 13:01:06 +0000

    Revert "Exclude mu-plugins"
    This reverts commit 1900c2ea3c7cc92d50cdbb55a3ee93dd1b1ef566.

--- wp-content/themes/lonelinesshub/inc/theme-frontend.php
+++ wp-content/themes/lonelinesshub/inc/theme-frontend.php
@@ -7,9 +7,10 @@
 
 defined( 'ABSPATH' ) || exit;
 
+
 /*
  * Custom excerpt funtion for posts shortcodes.
- *
+ * 
  */
 function get_excerpt_trim($num_words='8', $more=' ...'){
     $excerpt = get_the_excerpt();
@@ -17,10 +18,11 @@ function get_excerpt_trim($num_words='8', $more=' ...'){
     return $excerpt;
 }
 
+
 /*
- *
+ * 
  * Add script on footer for website rgistration form
- *
+ * 
 */
 add_action('wp_footer', 'lonelinesshub_function_name', 9999);
 function lonelinesshub_function_name(){
@@ -28,11 +30,11 @@ function lonelinesshub_function_name(){
 <script>
 jQuery( document ).ready( function($){
 	$( ".register-section-logo.private-on-div" ).empty();
-});
+});		
 </script>
 <style>
 .register-section-logo.private-on-div {
-	background-image: url(/wp-content/themes/lonelinesshub/assets/img/hub-logo.png) !important;
+	background-image: url(/wp-content/uploads/2021/02/hub-logo.png) !important;
 	width: 320px !important;
 	background-size: 310px !important;
 	height: 150px;
@@ -40,30 +42,11 @@ jQuery( document ).ready( function($){
 }
 .register .site-content {
 	background: #ffffff;
-}
+}	
 #privacy-modal header,
 #terms-modal header{
     display: none !important;
-}
+}	
 </style>
 <?php
 };
-
-
-/**
- * Fallback post image.
- *
- * @param  string $html The Output HTML of the post thumbnail
- * @param  int $post_id The post ID
- * @param  int $post_thumbnail_id The attachment id of the image
- * @param  string $size The size requested or default
- * @param  mixed string/array $attr Query string or array of attributes
- * @return string $html the Output HTML of the post thumbnail
- */
-function lonelinesshub_post_thumbnail_fb( $html, $post_id, $post_thumbnail_id, $size, $attr ) {
-  if ( empty( $html ) ) {
-    return '<img src="' . get_stylesheet_directory_uri() . '/assets/img/post-fallback-image.png" width="1000px" height="1000px" class="post-fallback-image" />';
-  }
-  return $html;
-}
-add_filter( 'post_thumbnail_html', 'lonelinesshub_post_thumbnail_fb', 20, 5 );
