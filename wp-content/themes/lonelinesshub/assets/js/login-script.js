d97168a1d679f61a347ea5659abc1c331f8cc4e9
Nuno <nuno@nuno-sarmento.com>
2021-05-14 23:48:45 +0100

    login script - password policy
    login script - password policy

---
+++ wp-content/themes/lonelinesshub/assets/js/login-script.js
@@ -0,0 +1,5 @@
+jQuery( document ).ready( function($){
+
+ $("#resetpassform").prepend('<div class="password-policy"><br><h4 style="margin-bottom:15px;font-size:22px;">Password policy:</h4><span style="font-size: 16px;">- Be at least 8 eight characters long</span><br><span style="font-size: 16px;">- Contain special characters e.g. !£$/</span><br><span style="font-size: 16px;">- Contain upper and lower case characters</span><br><span style="font-size: 16px;">- Contain a number</span><br><br></div>');		
+			
+});
