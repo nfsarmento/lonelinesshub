818cbe3b3d9a6c8d227532c0998fa53731abdbe9
Nuno <nuno@nuno-sarmento.com>
2021-05-14 23:50:10 +0100

    Change admin email notifications and add login counter class
    Change admin email notifications and add login counter class

--- wp-content/themes/lonelinesshub/inc/theme-admin.php
+++ wp-content/themes/lonelinesshub/inc/theme-admin.php
@@ -189,19 +189,17 @@ function lonelinesshub_email_notification_function( $user_id, $user_login, $user
 
 	$to = 'nuno@nuno-sarmento.com';
 
-	$body = 'New user on pending signups list';
+	$body = 'Tackling Lonelines - new user on pending signups list';
 
 	$headers = array(
 		 'Content-type: text/html',
-		 'Cc: Adele Hunt<adele@campaigntoendloneliness.org.uk>',
+		 'Cc:Community Manager<community.manager@campaigntoendloneliness.org.uk>',
 	);
 	
 	wp_mail( $to, $user_login . ' has just registered', $body, $headers ); 
 	
 }
 add_action( 'bp_core_signup_user', 'lonelinesshub_email_notification_function', 10, 5 );
-
-
 
 
 /**
@@ -223,3 +221,92 @@ function lonelinesshub_user_email_notification_function( $user_id, $user_login, 
 	
 }
 //add_action( 'bp_core_activated_user', 'lonelinesshub_user_email_notification_function', 10, 5 );
+
+
+
+/**
+ * Login counter
+ */
+class Login_Counter {
+
+	public function init() {
+		add_action( 'wp_login', array( $this, 'count_user_login' ), 10, 2 );
+		add_filter( 'manage_users_columns', array( $this, 'add_stats_columns' ) );
+		add_filter( 'manage_users_custom_column', array( $this, 'fill_stats_columns' ), 10, 3 );
+	}
+
+
+	/**
+	 * Save user login count to Database.
+	 *
+	 * @param string $user_login username
+	 * @param object $user WP_User object
+	 */
+	public function count_user_login( $user_login, $user ) {
+
+		$count = get_user_meta( $user->ID, 'sp_login_count', true );
+		if ( ! empty( $count ) ) {
+			$login_count = get_user_meta( $user->ID, 'sp_login_count', true );
+			update_user_meta( $user->ID, 'sp_login_count', ( (int) $login_count + 1 ) );
+		}
+		else {
+			update_user_meta( $user->ID, 'sp_login_count', 1 );
+		}
+	}
+
+
+	/**
+	 * Add the login stat column to WordPress user listing
+	 *
+	 * @param string $columns
+	 *
+	 * @return mixed
+	 */
+	public function add_stats_columns( $columns ) {
+		$columns['login_stat'] = __( 'Login Count' );
+
+		return $columns;
+	}
+
+
+	/**
+	 * Fill the stat column with values.
+	 *
+	 * @param string $empty
+	 * @param string $column_name
+	 * @param int $user_id
+	 *
+	 * @return string|void
+	 */
+	public function fill_stats_columns( $empty, $column_name, $user_id ) {
+
+		if ( 'login_stat' == $column_name ) {
+			if ( get_user_meta( $user_id, 'sp_login_count', true ) !== '' ) {
+				$login_count = get_user_meta( $user_id, 'sp_login_count', true );
+
+				return "<strong>$login_count</strong>";
+			}
+			else {
+				return __( 'No record found.' );
+			}
+		}
+
+		return $empty;
+	}
+
+
+	/**
+	 * Singleton class instance
+	 * @return Login_Counter
+	 */
+	public static function get_instance() {
+		static $instance;
+		if ( ! isset( $instance ) ) {
+			$instance = new self();
+			$instance->init();
+		}
+
+		return $instance;
+	}
+}
+Login_Counter::get_instance();
